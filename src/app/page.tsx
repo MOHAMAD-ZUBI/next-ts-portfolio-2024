40a35b59dc11f99f7e1450d9f1fd7edacd4331c5
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-06-11 03:09:35 +0300

    added intro component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,45 @@
+"use client";
 import About from "@/components/Main/About";
 import Contact from "@/components/Main/Contact";
 import Experience from "@/components/Main/Experience";
 import Hero from "@/components/Main/Hero";
+import Intro from "@/components/Main/Intro";
 import Projects from "@/components/Main/Projects";
 import StickyContact from "@/components/Main/StickyContact";
 import AnimatedSection from "@/components/Reusable/AnimatedSection";
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+  const [showMessages, setShowMessages] = React.useState(true);
+
+  const handleShowMessages = () => {
+    setInterval(() => {
+      setShowMessages(false);
+    }, 2400);
+  };
+  handleShowMessages();
   return (
-   <main className="min-h-screen bg-main-page main-page-specific ">
-   <div className="flex flex-col gap-[50px]">
-   <StickyContact/>
-    <AnimatedSection id="hero" >
-      <Hero/>
-    </AnimatedSection>
-    <AnimatedSection id="about" >
-    <About />
-    </AnimatedSection>
-    <AnimatedSection id="exp" >
-    <Experience />
-    </AnimatedSection>
-    <AnimatedSection id="work">
-    <Projects/>
-    </AnimatedSection>
-    <AnimatedSection id="contact" >
-    <Contact />
-    </AnimatedSection>
-   </div>
-    
-    
+    <main className="min-h-screen bg-main-page main-page-specific ">
+      <div className="flex flex-col gap-[50px]">
+        {showMessages && <Intro />}
+        <StickyContact />
+        <AnimatedSection id="hero">
+          <Hero />
+        </AnimatedSection>
+        <AnimatedSection id="about">
+          <About />
+        </AnimatedSection>
+        <AnimatedSection id="exp">
+          <Experience />
+        </AnimatedSection>
+        <AnimatedSection id="work">
+          <Projects />
+        </AnimatedSection>
+        <AnimatedSection id="contact">
+          <Contact />
+        </AnimatedSection>
+      </div>
     </main>
   );
 }
---
+++ src/components/Main/Intro.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useEffect } from "react";
+
+type Props = {};
+
+const Intro = (props: Props) => {
+  const [messageIndex, setMessageIndex] = React.useState(0);
+  const messages = [
+    "Hello,",
+    "Bonjour",
+    "Hola",
+    "Ciao",
+    "Hallo",
+    "Salut",
+    "Hej",
+    "你好",
+  ];
+
+  useEffect(() => {
+    if (messageIndex < messages.length) {
+      const interval = setInterval(() => {
+        setMessageIndex((prevIndex) => prevIndex + 1);
+      }, 300);
+      return () => clearInterval(interval); // Clear interval on component unmount
+    }
+  }, [messageIndex]);
+
+  return (
+    <div className="h-screen w-full text-white">
+      <div className="flex justify-center items-center h-full">
+        {messageIndex < messages.length && (
+          <h4 className="text-5xl capitalize text-white font-bold">
+            {messages[messageIndex]}
+          </h4>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
