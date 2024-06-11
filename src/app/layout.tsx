20815988e0061dd35805ae4514c3356c256f8bd6
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-06-11 16:37:32 +0300

    chore: Remove unused import in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
-import Header from "@/components/Main/Header";
+
 import SmoothScrolling from "@/components/Main/SmoothScrolling";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
