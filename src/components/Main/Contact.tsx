a5ee66f0a7cbb2a89ccdc0666fc300a73f217cbb
MOHAMAD-ZUBI <mozubi128@hotmail.com>
2024-06-18 04:01:16 +0300

    refactor: Update Contact component styles
    - Adjusted padding and font sizes for better responsiveness
    - Improved hover effects for better user experience

--- src/components/Main/Contact.tsx
+++ src/components/Main/Contact.tsx
@@ -65,7 +65,7 @@ const Contact = (props: Props) => {
           <div>
             <div
               onClick={handleClick}
-              className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden hover:cursor-pointer font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+              className="relative inline-flex items-center justify-center max-md:py-3 max-md:px-6  px-10 py-4 overflow-hidden hover:cursor-pointer font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
             >
               <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
               <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
@@ -76,7 +76,7 @@ const Contact = (props: Props) => {
             <Link
               href="https://mohamad-zubi.youcanbook.me/"
               target="_blank"
-              className="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+              className="relative inline-flex items-center justify-center px-10 py-4 max-md:py-3 max-md:px-6 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
             >
               <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
               <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
